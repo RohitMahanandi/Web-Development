a580d40e0c9f32a28abe2197dc6204ae5c1f3095
Rohitmahanandi <mrohit2002@gmail.com>
2023-08-31 22:28:29 +0530

    sfsa

---
+++ Binary Trees/bt.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+struct Node {
+    Node* left;
+    Node* right;
+    int data;
+};
+int main(int argc, char** argv) {
+    cout << "Hello world"<< endl;
+    
+}
